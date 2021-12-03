74836e4ae7604db077a412275a4e965cd268ea11
eehd80 <eehd80yul@gmail.com>
2021-12-03 13:46:48 +0900

    max-width

--- kyobobook/js/cover.js
+++ kyobobook/js/cover.js
@@ -1,48 +1,68 @@
-$(document).ready(function() {
-	$(window).scroll(function() {
-		var screenw = $(window).width();
-		var screenh = $('#header').outerHeight();
-		var articleh = $('#list').outerHeight();
-		var navh = $('#nav').outerHeight();
-		var sticky_top = articleh + navh - screenh;
-		var current_top = $(window).scrollTop();
-		var sectoin01_top = $('#section-01').outerHeight();
-		var sectoin02_top = sectoin01_top + $('#section-02').outerHeight();
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var screenw = $(window).width();
+        var screenh = $("#header").outerHeight();
+        var articleh = $("#list").outerHeight();
+        var navh = $("#nav").outerHeight();
+        var sticky_top = articleh + navh - screenh;
+        var current_top = $(window).scrollTop();
+        var sectoin01_top = $("#section-01").outerHeight();
+        var sectoin02_top = sectoin01_top + $("#section-02").outerHeight();
 
-		if (screenw > 991) {
-			if (current_top > sticky_top) {
-				$('#header').addClass('sticky');
-			} else {
-				$('#header').removeClass('sticky');
-			}
-			if (current_top < sectoin01_top) {
-				console.log('sectoin01_top : ' + sectoin01_top);
-				console.log('current_top : ' + current_top);
-				$('ul.cover-list li').removeClass('current-chapter');
-				$('ul.cover-list li:nth-child(1)').addClass('current-chapter');
-			} else if (current_top < sectoin02_top) {
-				$('ul.cover-list li').removeClass('current-chapter');
-				$('ul.cover-list li:nth-child(2)').addClass('current-chapter');
-			} else {
-				$('ul.cover-list li').removeClass('current-chapter');
-				$('ul.cover-list li:nth-child(3)').addClass('current-chapter');
-			}
-			$('.arrow-bottom').click(function() {
-				$(window).scrollTop(sectoin01_top + navh);
-			});
+        if (screenw > 991) {
+            if (current_top > sticky_top) {
+                $("#header").addClass("sticky");
+            } else {
+                $("#header").removeClass("sticky");
+            }
+            if (current_top < sectoin01_top) {
+                console.log("sectoin01_top : " + sectoin01_top);
+                console.log("current_top : " + current_top);
+                $("ul.cover-list li").removeClass("current-chapter");
+                $("ul.cover-list li:nth-child(1)").addClass("current-chapter");
+            } else if (current_top < sectoin02_top) {
+                $("ul.cover-list li").removeClass("current-chapter");
+                $("ul.cover-list li:nth-child(2)").addClass("current-chapter");
+            } else {
+                $("ul.cover-list li").removeClass("current-chapter");
+                $("ul.cover-list li:nth-child(3)").addClass("current-chapter");
+            }
+            $(".arrow-bottom").click(function () {
+                $(window).scrollTop(sectoin01_top + navh);
+            });
+        } else if (screenw < 992) {
+            var sticky_top = screenh;
+            if (current_top > sticky_top) {
+                $("#nav").addClass("sticky");
+                $("#list").css("margin-top", navh);
+            } else {
+                $("#nav").removeClass("sticky");
+                $("#list").css("margin-top", "0");
+            }
+            $(".arrow-bottom").click(function () {
+                $(window).scrollTop(screenh);
+            });
+        }
+    });
 
-		} else if (screenw < 992) {
-			var sticky_top = screenh;
-			if (current_top > sticky_top) {
-				$('#nav').addClass('sticky');
-				$('#list').css('margin-top', navh);
-			} else {
-				$('#nav').removeClass('sticky');
-				$('#list').css('margin-top', '0');
-			}
-			$('.arrow-bottom').click(function() {
-				$(window).scrollTop(screenh);
-			});
-		}
-	});
+    $(".cover-list a, .link-down").on("click", function (event) {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top,
+            },
+            900,
+            function () {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            },
+        );
+    });
 });
