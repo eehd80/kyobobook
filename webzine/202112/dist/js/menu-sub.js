1fa7c2fc1393378cec90d84d6ad41fd2bcede154
eehd80 <eehd80yul@gmail.com>
2021-12-08 22:59:28 +0900

    ddd

--- webzine/202112/dist/js/menu-sub.js
+++ webzine/202112/dist/js/menu-sub.js
@@ -15,36 +15,4 @@ $(document).ready(function () {
             $("#nav").removeClass("hover");
         },
     );
-
-    // var current_chapter = "Special Theme";
-
-    // if (current_chapter == "Special Theme") {
-    //     $("ul.cover-list li").removeClass("current-chapter");
-    //     $("ul.cover-list li:first-child").addClass("current-chapter");
-    // } else if (current_chapter == "미래를 향하다") {
-    //     $("ul.cover-list li").removeClass("current-chapter");
-    //     $("ul.cover-list li:nth-child(2)").addClass("current-chapter");
-    // } else {
-    //     $("ul.cover-list li").removeClass("current-chapter");
-    //     $("ul.cover-list li:nth-child(3)").addClass("current-chapter");
-    // }
-
-    $(".topbar-list .list-wrapper ul:first-child").mouseenter(function () {
-        $(".cover-list li:first-child span").addClass("on");
-    });
-    $(".topbar-list .list-wrapper ul:first-child").mouseleave(function () {
-        $(".cover-list li:first-child span").removeClass("on");
-    });
-    $(".topbar-list .list-wrapper ul:nth-child(2)").mouseenter(function () {
-        $(".cover-list li:nth-child(2) span").addClass("on");
-    });
-    $(".topbar-list .list-wrapper ul:nth-child(2)").mouseleave(function () {
-        $(".cover-list li:nth-child(2) span").removeClass("on");
-    });
-    $(".topbar-list .list-wrapper ul:nth-child(3)").mouseenter(function () {
-        $(".cover-list li:nth-child(3) span").addClass("on");
-    });
-    $(".topbar-list .list-wrapper ul:nth-child(3)").mouseleave(function () {
-        $(".cover-list li:nth-child(3) span").removeClass("on");
-    });
 });
