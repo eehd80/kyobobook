4a446aeebe83982aefa2fca14178e5eb8042adba
eehd80 <eehd80yul@gmail.com>
2021-12-08 15:33:46 +0900

    dd

--- webzine/202112/dist/js/menu-sub.js
+++ webzine/202112/dist/js/menu-sub.js
@@ -16,16 +16,16 @@ $(document).ready(function () {
         },
     );
 
-    var current_chapter = "Special Theme";
+    // var current_chapter = "Special Theme";
 
-    if (current_chapter == "Special Theme") {
-        $("ul.cover-list li").removeClass("current-chapter");
-        $("ul.cover-list li:first-child").addClass("current-chapter");
-    } else if (current_chapter == "미래를 향하다") {
-        $("ul.cover-list li").removeClass("current-chapter");
-        $("ul.cover-list li:nth-child(2)").addClass("current-chapter");
-    } else {
-        $("ul.cover-list li").removeClass("current-chapter");
-        $("ul.cover-list li:nth-child(3)").addClass("current-chapter");
-    }
+    // if (current_chapter == "Special Theme") {
+    //     $("ul.cover-list li").removeClass("current-chapter");
+    //     $("ul.cover-list li:first-child").addClass("current-chapter");
+    // } else if (current_chapter == "미래를 향하다") {
+    //     $("ul.cover-list li").removeClass("current-chapter");
+    //     $("ul.cover-list li:nth-child(2)").addClass("current-chapter");
+    // } else {
+    //     $("ul.cover-list li").removeClass("current-chapter");
+    //     $("ul.cover-list li:nth-child(3)").addClass("current-chapter");
+    // }
 });
